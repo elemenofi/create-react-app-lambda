5cc7058763575f2992d538d02d816902c6630ed9
Manuel Aberg Cobo <manuel.aberg@naturalcycles.com>
2018-03-29 22:34:19 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,46 +1,127 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
+var handler = window.StripeCheckout.configure({
+  key: 'pk_test_Os3pXXfffhGJXmRqNMsTwt4R',
+  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+  locale: 'auto',
+  token: (token) => {
+    alert('token', token)
+    // You can access the token ID with `token.id`.
+    // Get the token ID to your server-side code for use.
+
+    // run lamba function to pay
   }
+});
 
-  handleClick = (e) => {
-    e.preventDefault();
-
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-  }
-
-  render() {
-    const {loading, msg} = this.state;
-
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
-  }
-}
+// Close Checkout on page navigation:
+window.addEventListener('popstate', function() {
+  handler.close();
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {products: null, productsDisplay: {}, zoomsDisplay: {}, zoomIndex: 0};
+  }
+
+  componentDidMount () {
+    console.log('Executing Stripe API request for products')
+    
+    fetch('/.netlify/functions/hello')
+      .then(response => response.json())
+      .then(json => this.processProducts(json));
+  }
+
+  processProducts (json) {
+    const products = JSON.parse(json.products)
+
+    console.log('Products received from Stripe', products)
+    
+    this.setState({products})
+
+    let productsDisplay = {}    
+
+    products.data.forEach((product) => {
+      productsDisplay[product.name] = products.data.filter(prod => prod.name === product.name)
+    })
+
+    this.setState({productsDisplay})
+  }
+
   render() {
+    let {productsDisplay} = this.state
+
+    let productsList = []
+    
+    productsList = this.processDisplayProducts(productsDisplay, productsList)
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LambdaDemo/>
+      <div className='productList'>
+        {productsList}
       </div>
     );
+  }
+
+  checkoutProduct (product) {
+    // run lambda function to create order
+
+
+    handler.open({
+      name: product.name,
+      description: product.skus.data[0].attributes.color,
+      currency: product.skus.data[0].currency,
+      amount: product.skus.data[0].price / 100
+    });
+  }
+
+  processDisplayProducts (productsDisplay, productsList) {
+    let {zoomsDisplay} = this.state
+    
+    Object.keys(productsDisplay).forEach(productName => {
+      productsList.push(<h1>{productName}</h1>)
+
+      const zoomIndex = zoomsDisplay[productName] || 0
+      const zoomProduct = productsDisplay[productName][zoomIndex]
+
+      // big product
+      productsList.push(
+        <div className='product'>
+          <img src={zoomProduct.images[0]} alt={zoomProduct.name}/>
+          <button onClick={this.checkoutProduct.bind(this, zoomProduct)}>Buy</button>
+        </div>
+      )
+
+      const thumbnails = []
+
+      // thumbnails
+      productsDisplay[productName].forEach(product => {
+        thumbnails.push(
+          <div 
+            onClick={this.handleClick.bind(this, product)} 
+            className='thumbnail'
+          >
+            <img src={product.images[0]} alt={product.name}/>
+          </div>       
+        )
+      })
+
+      productsList.push(<div className='thumbnailWrapper'>{thumbnails}</div>)
+
+      productsList.push(<div className='productsBreak'></div>)      
+    })
+
+    return productsList
+  }
+
+  handleClick (product) {
+    let {zoomsDisplay} = this.state
+
+    const zoomIndex = this.state.productsDisplay[product.name].indexOf(product)
+
+    zoomsDisplay[product.name] = zoomIndex
+
+    this.setState({zoomsDisplay})
   }
 }
 
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,23 @@
+var stripe = require("stripe")(
+  "sk_test_kkCVbDTXtYKdHO8wfaNpKH8o"
+);
+
+
+
 export function handler(event, context, callback) {
   console.log(event)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!"})
-  })
+
+  stripe.products.list(
+    { limit: 100 },
+    (err, products) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({products: JSON.stringify(products)})
+      })
+    }
+  );
+
+  
 }
+
+
